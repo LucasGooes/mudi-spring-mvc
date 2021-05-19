fe38aefc6cde4299ae9e9f546a43af16bbbc230e
LucasGooes <lucasmarcos343@gmail.com>
2021-05-19 01:11:35 -0400

    Ajustando a navegação

--- src/main/java/br/com/marinhosoftware/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/br/com/marinhosoftware/mvc/mudi/config/WebSecurityConfig.java
@@ -20,6 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/home/**")
+			.permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin( form -> form
@@ -27,8 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.defaultSuccessUrl("/usuario/pedido", true)
 				.permitAll()
 			)
-			.logout(logout -> logout.logoutUrl("/logout"))
-			.csrf().disable();
+			.logout(logout -> {
+				logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+			});
 	}
 	
 	@Override
--- src/main/java/br/com/marinhosoftware/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/marinhosoftware/mvc/mudi/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.marinhosoftware.mvc.mudi.model.Pedido;
+import br.com.marinhosoftware.mvc.mudi.model.StatusPedido;
 import br.com.marinhosoftware.mvc.mudi.repository.PedidoRepository;
 
 @Controller
@@ -21,7 +22,7 @@ public class HomeController {
 
 	@GetMapping
 	public String home(Model model, Principal principal) {
-		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
